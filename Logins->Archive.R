d39a4736b3ed97a2c4b36f130153fb79e03eace2
moffit <alex.moffit@gmail.com>
2015-03-05 17:07:57 -0800

    Made some changes to the scripts. Making sure that correct users are in the analysis. Also, did a explortory k-means clustering to see if anything showed up.

--- Logins->Archive.R
+++ Logins->Archive.R
@@ -6,6 +6,26 @@
 # Getting Q4 goals only
   Q4.archive.achieve <- archived.achieve %>%
     filter(end > "2014-09-30" & end < "2015-01-01")
+  
+# Getting users who have logged in in Q4
+  Q4.logins.count <- Q4.logins.achieve %>%
+    select(user_id, when) %>%
+    group_by(user_id) %>%
+    tally(sort=TRUE)
+  
+# Bucketing users into login buckets: less than monthly (n < 3), monthly (3=< n < 12), and weekly (12 < n)
+  less.than.monthly <- Q4.logins.count %>%
+    filter(n < 3)
+  less.than.monthly.users <- less.than.monthly$user_id
+  
+  monthly.login <- Q4.logins.count %>%
+    filter(n >= 3) %>%
+    filter(n < 12)
+  monthly.login.users <- monthly.login$user_id
+  
+  weekly.login <- Q4.logins.count %>%
+    filter(n >= 12)
+  weekly.login.users <- weekly.login$user_id
 
 # Getting Q4 goals from users who have logged in in Q1
   # subset.Q4.archive.achieve <- subset(Q4.archive.achieve, owner_id %in% active.users.achieve)
--- NoZLogins->Archive.R
+++ NoZLogins->Archive.R
@@ -9,6 +9,25 @@ no.z.archived.achieve <- no.delete.or.started.achieve %>%
 no.z.Q4.archive.achieve <- no.z.archived.achieve %>%
   filter(end > "2014-09-30" & end < "2015-01-01")
 
+# Getting users who have logged in in Q4
+Q4.no.z.logins.count <- Q4.no.z.logins %>%
+  select(user_id, when) %>%
+  group_by(user_id) %>%
+  tally(sort=TRUE)
+
+z.less.than.monthly <- Q4.no.z.logins.count %>%
+  filter(n < 3)
+z.less.than.monthly.users <- z.less.than.monthly$user_id
+
+z.monthly.login <- Q4.no.z.logins.count %>%
+  filter(n >= 3) %>%
+  filter(n < 12)
+z.monthly.login.users <- z.monthly.login$user_id
+
+z.weekly.login <- Q4.no.z.logins.count %>%
+  filter(n >= 12)
+z.weekly.login.users <- z.weekly.login$user_id
+
 # Getting Q4 goals from users who have logged in in Q1
 # subset.Q4.archive.achieve <- subset(Q4.archive.achieve, owner_id %in% active.users.achieve)
 # Getting Q4 goals from users who have logged in in Q4 and Q1
@@ -32,10 +51,30 @@ no.z.avg.archive.percent <- no.z.Q4.archive.achieve %>%
 # To change between the two, I change the first variable in subset()
 # For avg progess --> subset(avg.goals.percent, )
 # For % completed --> subset(testing.completed, )
-less.monthly.completion <- subset(no.z.avg.archive.percent, owner_id %in% less.than.monthly.users)
-monthly.completion <- subset(no.z.avg.archive.percent, owner_id %in% monthly.login.users)
-weekly.completion <- subset(no.z.avg.archive.percent, owner_id %in% weekly.login.users)
+  less.monthly.completion <- subset(no.z.avg.archive.percent, owner_id %in% z.less.than.monthly.users)
+  monthly.completion <- subset(no.z.avg.archive.percent, owner_id %in% z.monthly.login.users)
+  weekly.completion <- subset(no.z.avg.archive.percent, owner_id %in% z.weekly.login.users)
 
-summary(less.monthly.completion)
-summary(monthly.completion)
-summary(weekly.completion)
+  summary(less.monthly.completion)
+  # str(less.monthly.completion)
+  summary(monthly.completion)
+  # str(monthly.completion)
+  summary(weekly.completion)
+  # str(weekly.completion)
+
+write.csv(less.monthly.completion, file = "LessMonthlyCompletion.csv", na = "")
+boxplot(less.monthly.completion$avg.progress, monthly.completion$avg.progress, weekly.completion$avg.progress)
+
+# Dumbest stuff ever. I hate this wrangling. Getting login count and avg. progress into the same data frame
+  dumb.variable <- subset(no.z.avg.archive.percent, owner_id %in% Q4.no.z.logins.count.2$owner_id)
+  dumb.variable.2 <- subset(Q4.no.z.logins.count.2, owner_id %in% no.z.avg.archive.percent$owner_id)
+  
+  dumb.variable.2 <- dumb.variable.2 %>%
+    arrange(owner_id)
+  dumb.variable[, "logins"] <- dumb.variable.2$n
+  dumbest.variable[, "owner_id"] <- NULL
+
+# Clustering
+goal.cluster <- kmeans(dumbest.variable, centers = 8)
+clusplot(dumbest.variable, goal.cluster$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
+goal.cluster
